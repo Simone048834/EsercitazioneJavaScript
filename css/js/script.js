0f153c87bf45a449db8b2b778ac1ca8470f7c3e6
Simone048834 <99796585+Simone048834@users.noreply.github.com>
2022-03-01 21:09:21 +0100

    Delete script.js

--- css/js/script.js
+++
@@ -1,9 +0,0 @@
-function funzioneProdotto()
-{
-    var numero1=document.forms["due_numeri"]["n1"].value;
-    var numero2=document.forms["due_numeri"]["n2"].value;
-        
-        risultato=numero1*numero2;
-        window.alert("Risultato: "  + risultato );
-        
-}
