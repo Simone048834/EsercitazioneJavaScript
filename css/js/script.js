ea2d901150d84c01d4c597345bdeb9bd827a6666
Simone048834 <99796585+Simone048834@users.noreply.github.com>
2022-03-01 20:56:59 +0100

    Add files via upload

---
+++ css/js/script.js
@@ -0,0 +1,9 @@
+function funzioneProdotto()
+{
+    var numero1=document.forms["due_numeri"]["n1"].value;
+    var numero2=document.forms["due_numeri"]["n2"].value;
+        
+        risultato=numero1*numero2;
+        window.alert("Risultato: "  + risultato );
+        
+}
